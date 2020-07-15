9fa8adfa7103103fea591ce2cd669959fbab5c74
ayushi0408 <66661771+ayushi0408@users.noreply.github.com>
2020-07-15 16:11:32 +0530

    Create index.php

---
+++ assignment2/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Ivan Neradovic</title>
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	</head>
+	<body>
+		<div class="container">
+			<h1>Welcome to Autos Database</h1>
+			<p>
+				<a href="login2.php">Please Log In</a>
+			</p>
+			<p>
+				Attempt to go to 
+				<a href="ass.php">ass.php</a> 
+				without logging in - it should fail with an error message.
+			</p>
+			<p>
+				<a href="https://www.wa4e.com/assn/" target="_blank">
+					Specification for this Application
+				</a>
+			</p>
+		</div>
+	</body>
+</html>
